c25fc83680ac29174fdaece2252229532989a60f
mcphail-alex <mcphail.alex@gmail.com>
2022-10-29 14:04:25 -0500

    a few frontend changes

--- NPM-MODULE/src/limiters/rate-limiter.ts
+++ NPM-MODULE/src/limiters/rate-limiter.ts
@@ -4,15 +4,15 @@ await redisClient.connect();
 
 export async function rateLimiter(user_ip: any, queryCost: number, rateLimit: number){
   let time = 10;
-  console.log(queryCost);
-  console.log(typeof queryCost)
-  console.log("rounded: ", Math.floor(Number(queryCost)));
+  // console.log(queryCost);
+  // console.log(typeof queryCost)
+  // console.log("rounded: ", Math.floor(Number(queryCost)));
   
   let rounded = Math.floor(queryCost);
   let totalCost = await redisClient.incrBy(user_ip, rounded);
-  console.log(totalCost, " ", typeof totalCost);
+  // console.log(totalCost, " ", typeof totalCost);
   if (Number(totalCost) === rounded) redisClient.expire(user_ip, time);
-  console.log(Number(totalCost), rateLimit);
+  // console.log(Number(totalCost), rateLimit);
   if (Number(totalCost) >= rateLimit) return false;
   else return true;
 }
